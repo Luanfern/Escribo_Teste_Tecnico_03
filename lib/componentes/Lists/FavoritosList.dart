9d2dd18f25979a69d9c73269ee6a9620c55ae554
LuanPegador07 <51713644+Luanfern@users.noreply.github.com>
2022-01-28 19:01:28 -0300

    decima oitava commit - ajustes e favovitos salvando no SQLITE

--- lib/componentes/Lists/FavoritosList.dart
+++ lib/componentes/Lists/FavoritosList.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:starwars_escribo/componentes/Lists/Componentes%20-%20Lista/CardLists.dart';
@@ -11,19 +14,46 @@ class ListFavoritos extends StatefulWidget {
 }
 
 class _ListFavoritosState extends State<ListFavoritos> {
-
   @override
   Widget build(BuildContext context) {
     final GerenciamentodeFavoritos favoritos = Provider.of(context);
-    return ListView.builder(
-        itemCount: favoritos.getListfavoritos.length,
-        itemBuilder: (ctx, i) {
-          return  CardLists(
-            corCard: favoritos.getListfavoritos[i].type! == 'Filme' ? const Color.fromARGB(255, 150, 0, 0): const Color.fromARGB(255, 0, 150, 0),
-            name: favoritos.getListfavoritos[i].name!,
-            type: favoritos.getListfavoritos[i].type!,
-            fav: true,
-          );
-        });
+    return favoritos.getListfavoritos.isEmpty
+        ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/imgs/carregandogif.gif',
+                  scale: 5,
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:const [
+                     Text(
+                      'Sem favoritos',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 15,),
+                    Icon(Icons.favorite,color: Colors.red,)
+                  ],
+                )
+              ],
+            ),
+          )
+        : ListView.builder(
+            itemCount: favoritos.getListfavoritos.length,
+            itemBuilder: (ctx, i) {
+              return CardLists(
+                corCard: favoritos.getListfavoritos[i].type! == 'Filme'
+                    ? const Color.fromARGB(255, 150, 0, 0)
+                    : const Color.fromARGB(255, 0, 150, 0),
+                name:
+                    utf8.decode(favoritos.getListfavoritos[i].name!.codeUnits),
+                type: favoritos.getListfavoritos[i].type!,
+                fav: true,
+              );
+            });
   }
 }
